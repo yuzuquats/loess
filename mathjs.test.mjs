dd874e4a918b076c0006e7305276c7a50e5cea2f
James Kao <james.l.kao@gmail.com>
2022-11-29 18:55:09 -0800

    Add other test stubs

--- mathjs.test.mjs
+++ mathjs.test.mjs
@@ -1,13 +1,37 @@
 import { MathJs } from "./mathjs.mjs";
 import { assertEqFloat, assertError } from "./assert.mjs";
 function test(library) {
+    const tests = {
+        std: () => {
+            assertError(() => library.std([]));
+            assertEqFloat(library.std([1]), 0);
+            assertEqFloat(library.std([3]), 0);
+            assertEqFloat(library.std([1, 2, 3, 4]), 1.2909944487358056);
+            assertEqFloat(library.std([1, 2, 3, 4, 5]), 1.5811388300841898);
+            assertEqFloat(library.std([1, 1, 1, 1, 1]), 0);
+        },
+        dotMultiply: () => { },
+        sum: () => { },
+        multiply: () => { },
+        subtract: () => { },
+        square: () => { },
+        squeeze: () => { },
+        inv: () => { },
+        matrix: () => { },
+        mean: () => { },
+        transpose: () => { },
+        euclideanDist: () => { },
+        distMatrix: () => { },
+        round: () => { },
+        abs: () => { },
+        median: () => { },
+        zeroes: () => { },
+        ones: () => { },
+        sort: () => { },
+        size: () => { },
+    };
     console.log("  TEST: std");
-    assertError(() => library.std([]));
-    assertEqFloat(library.std([1]), 0);
-    assertEqFloat(library.std([3]), 0);
-    assertEqFloat(library.std([1, 2, 3, 4]), 1.2909944487358056);
-    assertEqFloat(library.std([1, 2, 3, 4, 5]), 1.5811388300841898);
-    assertEqFloat(library.std([1, 1, 1, 1, 1]), 0);
+    tests.std();
 }
 console.log("Testing Original Library (MathJs)");
 test(MathJs);
